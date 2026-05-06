c112dd0014451c963c992e207d64c57f70e18bde
Paulo César Monteiro <paulocsm2006@gmail.com>
2026-05-06 18:22:06 -0300

    feat: adiciona aba de condições no arquivo html, cria o documento detalhes_beneficios.js e simula uma base de dados com json

---
+++ codigo/public/modulos/login/paulocesarmonteiro/js/detalhes_beneficios.js
@@ -0,0 +1,27 @@
+const dados = {
+  "beneficios": [
+    {
+      "id": "BEN-001",
+      "nome": "Bolsa Família",
+      "categoria": "alimentacao",
+      "descricao": "Auxílio mensal para famílias de baixa renda cadastradas em programas sociais.",
+      "requisitos": [
+        "A soma de todos os rendimentos da casa, dividida pelo número de moradores, não deve passar de R$ 218.",
+        "É obrigatório estar inscrito no Cadastro Único para Programas Sociais do Governo Federal.",
+        "O cadastro deve estar atualizado (no máximo a cada 2 anos) no CRAS (Centro de Referência de Assistência Social)."
+      ],
+      "condicoes": [
+        "Frequência escolar mínima para crianças e adolescentes (4 a 17 anos). ",
+        "Acompanhamento do pré-natal para gestantes; acompanhamento nutricional (peso e altura) das crianças menores de 7 anos; cumprimento do calendário nacional de vacinação."
+      ],
+      "valorBase": 142.00,
+      "dataInicio": "2025-01-01",
+      "dataFim": "2025-12-31",
+      "prazoInscricao": null,
+      "publicoAlvo": [
+        "BAIXA_RENDA"
+      ],
+      "orgaoResponsavel": "Governo Federal"
+    }
+ ]
+}
